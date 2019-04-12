41ea6977c80ff0981d204c38c0eeb5b21ba6434c
JayaKrishna <namburu1995@gmail.com>
2019-04-12 12:37:52 +0530

    Made tsconfig to be independent for all packages

--- packages/package_two/dist/index.js
+++ packages/package_two/dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var packageTwo = function () {
     console.log('This is from package two');
+    console.log('Made a change in the second package');
 };
 exports.default = packageTwo;
 //# sourceMappingURL=index.js.map
--- packages/package_two/lib/index.ts
+++ packages/package_two/lib/index.ts
@@ -1,5 +1,6 @@
 const packageTwo = () => {
     console.log('This is from package two')
+    console.log('Made a change in the second package')
 }
 
 export default packageTwo
